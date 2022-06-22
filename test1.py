42985719cea40ddaea5ac21c0c1f031c31fa76fa
whitePigeon <81309180+pkasprzak9@users.noreply.github.com>
2022-06-22 17:07:59 +0200

    Dodaje dwa cool pliki

---
+++ test1.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+def submit():
+    password = entry.get()
+    print("Twoje hasło to: " + password)
+
+def delete():
+    entry.delete(0, END)
+    print('Text has been deleted')
+
+def backspace():
+    entry.delete((len(entry.get())-1), END)
+
+window = Tk()
+
+window.title('Enter password')
+
+entry = Entry(window,
+              font=('arial',
+                    30),
+              show='*',
+              fg='#00FF00',
+              bg='black')
+entry.pack(side=LEFT)
+
+submit_button = Button(window, text='submit', command=submit)
+submit_button.pack(side=RIGHT)
+
+delete_button = Button(window, text='delete', command=delete)
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(window, text='backspace', command=backspace)
+backspace_button.pack(side=RIGHT)
+
+
+
+window.mainloop()
---
+++ test2.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+#   count = 0
+
+def click():
+    #  global count
+    #  count+=1
+    print('Stop it!')
+
+window = Tk()
+
+#window.geometry("500x500")
+window.title("test2")
+window.config(background="#9938C0")
+
+# label = Label(window, text="Kłaniam się nisko",
+#              background="#9938C0",
+#              font=('Arial',30,'bold'),
+#              fg='#F59B09',
+#              relief=RAISED,
+#              bd=5,)
+
+button = Button(window,
+                text="Don't click me!",
+                font=('Comic Sands', 30),
+                fg='#F59B09',
+                highlightbackground='#9938C0',
+                activeforeground='#F59B09',
+                command=click)
+
+button.pack()
+# label.pack()
+# label.place(x=200, y=200)
+
+window.mainloop()
